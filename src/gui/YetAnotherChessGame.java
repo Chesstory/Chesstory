9973b6b23def0d901403a1dd32e8ee9c06f76068
Baptiste Guillemot <b.guillemot95@gmail.com>
2015-12-07 12:56:24 +0100

    Ajout de la methode pour les deplacements

--- src/gui/YetAnotherChessGame.java
+++ src/gui/YetAnotherChessGame.java
@@ -17,273 +17,294 @@ import java.util.logging.Logger;
  * @author samuel
  */
 import java.util.ArrayList;
+
 public class YetAnotherChessGame extends JFrame implements MouseListener, MouseMotionListener {
 
-    /**
-     * Paneau de fond
-     */
-    JLayeredPane layeredPane;
+	/**
+	 * Paneau de fond
+	 */
+	JLayeredPane layeredPane;
 
-    /**
-     * Echiquier
-     */
-    JPanel chessBoard;
+	/**
+	 * Echiquier
+	 */
+	JPanel chessBoard;
 
-    /**
-     * Une pièce
-     */
-    JLabel chessPiece;
+	/**
+	 * Une pièce
+	 */
+	JLabel chessPiece;
 
-    /**
-     * Déplacement en x
-     */
-    int xDeplace;
-    /**
-     * Déplacement en y
-     */
-    int yDeplace;
+	/**
+	 * Déplacement en x
+	 */
+	int xDeplace;
+	/**
+	 * Déplacement en y
+	 */
+	int yDeplace;
 
-    /**
-     * position de départ
-     */
-    Position depart;
-    /**
-     * position d'arrivée
-     */
-    Position arrive;
+	/**
+	 * position de départ
+	 */
+	Position depart;
+	/**
+	 * position d'arrivée
+	 */
+	Position arrive;
 
-    /**
-     * L'échiquier courrant
-     */
-    static Echiquier ech;
+	/**
+	 * L'échiquier courrant
+	 */
+	static Echiquier ech;
 
-    private boolean first = true;
+	private boolean first = true;
 
-    /**
-     * Efface tout l'échiquier
-     */
-    private void videEchiquier() {
-        for (int j = 0; j < ech.getDimY(); j++) {
-            for (int i = 0; i < ech.getDimX(); i++) {
-                JPanel panel = (JPanel) chessBoard.getComponent(j * ech.getDimX() + i);
-                panel.removeAll();
-                panel.repaint();
-            }
-        }
-    }
+	/**
+	 * Efface tout l'échiquier
+	 */
+	private void videEchiquier() {
+		for (int j = 0; j < ech.getDimY(); j++) {
+			for (int i = 0; i < ech.getDimX(); i++) {
+				JPanel panel = (JPanel) chessBoard.getComponent(j * ech.getDimX() + i);
+				panel.removeAll();
+				panel.repaint();
+			}
+		}
+	}
 
-    /**
-     * Dessine intégralement toutes les pièces de l'échiquier
-     */
-    private void dessineToutesLesPieces() {
-        // dessin des pièces
-        for (int j = 0; j < ech.getDimY(); j++) {
-            for (int i = 0; i < ech.getDimX(); i++) {
-                Piece p = ech.getPieceCase(i, 8 - j - 1);
+	/**
+	 * Dessine intégralement toutes les pièces de l'échiquier
+	 */
+	private void dessineToutesLesPieces() {
+		// dessin des pièces
+		for (int j = 0; j < ech.getDimY(); j++) {
+			for (int i = 0; i < ech.getDimX(); i++) {
+				Piece p = ech.getPieceCase(i, 8 - j - 1);
 
-                if (p != null) {
-                    String nom = p.getNom();
-                    String nomComplet = "icons/" + nom + ".png";
-                    //java.net.URL imgURL = getClass().getResource(nomComplet);
-                    ClassLoader cl = this.getClass().getClassLoader();
-                    JLabel piece = new JLabel(new ImageIcon(cl.getResource(nomComplet)));
-                    JPanel panel = (JPanel) chessBoard.getComponent(j * ech.getDimX() + i);
-                    panel.add(piece);
-                }
-            }
-        }
-    }
+				if (p != null) {
+					String nom = p.getNom();
+					String nomComplet = "icons/" + nom + ".png";
+					// java.net.URL imgURL = getClass().getResource(nomComplet);
+					ClassLoader cl = this.getClass().getClassLoader();
+					JLabel piece = new JLabel(new ImageIcon(cl.getResource(nomComplet)));
+					JPanel panel = (JPanel) chessBoard.getComponent(j * ech.getDimX() + i);
+					panel.add(piece);
+				}
+			}
+		}
+	}
 
-    /**
-     * Vide et dessine
-     */
-    private void redessine() {
-        videEchiquier();
-        dessineToutesLesPieces();
-    }
-    
-    private void surbrillance(Position pos, Color couleur){
-        JPanel panel = (JPanel) chessBoard.getComponent((8 - pos.getY() -1) * ech.getDimX() + pos.getX());
-        panel.setBackground(couleur);
-    }
-    public void afficheLesPositionsDansLeGUI(Position p){
-    	ArrayList<Position> array=ech.affichePositionAccessibles(p);
-    	
-    	for(int i=0; i<array.size();i++){
-    		surbrillance(array.get(i), Color.ORANGE);
-    	}
-    }
-    private void dessinEchiquier(){
-        for (int j = 0; j < ech.getDimY(); j++) {
-            for (int i = 0; i < ech.getDimX(); i++) {
-                JPanel panel = (JPanel) chessBoard.getComponent(j * ech.getDimX() + i);
-                if (i % 2 == 0) {
-                    panel.setBackground(j % 2 == 0 ? Color.white : Color.getHSBColor(0.56f, 1.0f, 0.8f));
-                 } else {
-                    panel.setBackground(j % 2 == 0 ? Color.getHSBColor(0.56f, 1.0f, 0.8f) : Color.white);
-                } 
-            }
-        }
-    }
-    
-    /**
-     * Constructeur
-     */
-    public YetAnotherChessGame(String fenDeDeapart) {
-    	ech = new Echiquier();
-        ech.setFEN(fenDeDeapart);
-        
-        Dimension boardSize = new Dimension(600, 600);
+	/**
+	 * Vide et dessine
+	 */
+	private void redessine() {
+		videEchiquier();
+		dessineToutesLesPieces();
+	}
 
-        //  Use a Layered Pane for this this application
-        layeredPane = new JLayeredPane();
-       // panel.add(layeredPane);
-        
-        
-        layeredPane.setPreferredSize(boardSize);
-        layeredPane.addMouseListener(this);
-        layeredPane.addMouseMotionListener(this);
+	private void surbrillance(Position pos, Color couleur) {
+		JPanel panel = (JPanel) chessBoard.getComponent((8 - pos.getY() - 1) * ech.getDimX() + pos.getX());
+		panel.setBackground(couleur);
+	}
 
-        //Add a chess board to the Layered Pane
-        chessBoard = new JPanel();
-        layeredPane.add(chessBoard, JLayeredPane.DEFAULT_LAYER);
-        chessBoard.setLayout(new GridLayout(8, 8));
-        chessBoard.setPreferredSize(boardSize);
-        chessBoard.setBounds(0, 0, boardSize.width, boardSize.height);
+	public void afficheLesPositionsDansLeGUI(Position p) {
+		ArrayList<Position> array = ech.affichePositionAccessibles(p);
 
-        // dessin de l'équiquier
-        for (int i = 0; i < 64; i++) {
-            JPanel square = new JPanel(new BorderLayout());
-            chessBoard.add(square);
+		for (int i = 0; i < array.size(); i++) {
+			surbrillance(array.get(i), Color.ORANGE);
+		}
+	}
 
-            int row = (i / 8) % 2;
-            if (row == 0) {
-                square.setBackground(i % 2 == 0 ? Color.white : Color.getHSBColor(0.56f, 1.0f, 0.8f));
-            } else {
-                square.setBackground(i % 2 == 0 ? Color.getHSBColor(0.56f, 1.0f, 0.8f) : Color.white);
-            }
-        }
-        dessineToutesLesPieces();
-    }
+	private void dessinEchiquier() {
+		for (int j = 0; j < ech.getDimY(); j++) {
+			for (int i = 0; i < ech.getDimX(); i++) {
+				JPanel panel = (JPanel) chessBoard.getComponent(j * ech.getDimX() + i);
+				if (i % 2 == 0) {
+					panel.setBackground(j % 2 == 0 ? Color.white : Color.getHSBColor(0.56f, 1.0f, 0.8f));
+				} else {
+					panel.setBackground(j % 2 == 0 ? Color.getHSBColor(0.56f, 1.0f, 0.8f) : Color.white);
+				}
+			}
+		}
+	}
 
-    /**
-     * Méthode appelée lorsque la souris est cliquée
-     *
-     * @param e
-     */
-    @Override
-    public void mousePressed(MouseEvent e) {
-    }
-    @Override
-    public void mouseDragged(MouseEvent me) {
-    }
-    @Override
-    public void mouseReleased(MouseEvent e) {
-    }
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        if (!first) {
-            Component c = chessBoard.findComponentAt(e.getX(), e.getY());
-            chessPiece.setVisible(false);
+	/**
+	 * Verification et appliction d'un deplacement
+	 * 
+	 * @param move Le déplacement
+	 * @param c
+	 * @return La validité du déplacement
+	 */
+	public boolean makeDeplacement(Deplacement move, Component c) {
+		if (ech.estValideDeplacement(move)) {
+			ech.executeDeplacement(move);
+			if (ech.isPromotion()) {
+				String nom = ech.getPiece(move.getArrive()).getNom();
+				Container parent = (Container) c;
 
-            JPanel panel = (JPanel) chessBoard.getComponent((8 - depart.getY() -1) * ech.getDimX() + depart.getX());
-            
-            arrive = new Position((int) ((e.getX() / 600.0) * 8.0), (int) ((((600.0 - e.getY()) / 600.0) * 8.0)));
-            //here we have to save the color and the piece into d for later save it in the arraylist
-            
-            Deplacement d = new Deplacement(depart, arrive, ech.getPiece(depart).getCode(), ech.getPiece(depart).getColor());
+				chessPiece = new JLabel(new ImageIcon("icons/" + nom + ".png"));
+				parent.add(chessPiece);
+			}
 
-            System.out.println("==> Déplacement : " + d);
+			// cas d'une prise
+			if (c instanceof JLabel) {
+				Container parent = c.getParent();
+				parent.remove(0);
+				parent.add(chessPiece);
+			} else {
+				Container parent = (Container) c;
+				parent.add(chessPiece);
+			}
 
-            if (ech.estValideDeplacement(d)) {
-                ech.executeDeplacement(d);
-                if (ech.isPromotion()) {
-                    String nom = ech.getPiece(d.getArrive()).getNom();
-                    Container parent = (Container) c;
+			if (ech.petitRoqueEnCours() || ech.grandRoqueEnCours() || ech.priseEnPassantEnCours()) {
+				redessine();
+			}
 
-                    chessPiece = new JLabel(new ImageIcon("icons/" + nom + ".png"));
-                    parent.add(chessPiece);
-                }
+			chessPiece.setVisible(true);
+			return true;
+		} else
+			return false;
+	}
 
-                // cas d'une prise
-                if (c instanceof JLabel) {
-                    Container parent = c.getParent();
-                    parent.remove(0);
-                    parent.add(chessPiece);
-                } else {
-                    Container parent = (Container) c;
-                    parent.add(chessPiece);
-                }
+	/**
+	 * Constructeur
+	 */
+	public YetAnotherChessGame(String fenDeDeapart) {
+		ech = new Echiquier();
+		ech.setFEN(fenDeDeapart);
 
-                if (ech.petitRoqueEnCours() || ech.grandRoqueEnCours()
-                        || ech.priseEnPassantEnCours()) {
-                    redessine();
-                }
+		Dimension boardSize = new Dimension(600, 600);
 
-                chessPiece.setVisible(true);
+		// Use a Layered Pane for this this application
+		layeredPane = new JLayeredPane();
+		// panel.add(layeredPane);
 
-            } else {
-                // replacer sur la case de départ
-                panel.add(chessPiece);
-                chessPiece.setVisible(true);
-            }
+		layeredPane.setPreferredSize(boardSize);
+		layeredPane.addMouseListener(this);
+		layeredPane.addMouseMotionListener(this);
 
-            //Remettre la couleur d'origine
-            dessinEchiquier();
+		// Add a chess board to the Layered Pane
+		chessBoard = new JPanel();
+		layeredPane.add(chessBoard, JLayeredPane.DEFAULT_LAYER);
+		chessBoard.setLayout(new GridLayout(8, 8));
+		chessBoard.setPreferredSize(boardSize);
+		chessBoard.setBounds(0, 0, boardSize.width, boardSize.height);
 
-            first = true;
-        } else {
-            chessPiece = null;
-            Component c = chessBoard.findComponentAt(e.getX(), e.getY());
-            // si la case est vide : rien à faire
-            if (c instanceof JPanel) {
-                return;
-            }
+		// dessin de l'équiquier
+		for (int i = 0; i < 64; i++) {
+			JPanel square = new JPanel(new BorderLayout());
+			chessBoard.add(square);
 
-            // retrouver la case correspondante
-            depart = new Position((int) ((e.getX() / 600.0) * 8.0), (int) ((((600.0 - e.getY()) / 600.0) * 8.0)));
+			int row = (i / 8) % 2;
+			if (row == 0) {
+				square.setBackground(i % 2 == 0 ? Color.white : Color.getHSBColor(0.56f, 1.0f, 0.8f));
+			} else {
+				square.setBackground(i % 2 == 0 ? Color.getHSBColor(0.56f, 1.0f, 0.8f) : Color.white);
+			}
+		}
+		dessineToutesLesPieces();
+	}
 
-            System.out.print(depart);
+	/**
+	 * Méthode appelée lorsque la souris est cliquée
+	 *
+	 * @param e
+	 */
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
 
-            Point parentLocation = c.getParent().getLocation();
+	@Override
+	public void mouseDragged(MouseEvent me) {
+	}
 
-            xDeplace = parentLocation.x - e.getX();
-            yDeplace = parentLocation.y - e.getY();
-            chessPiece = (JLabel) c;
-            chessPiece.setLocation(e.getX() + xDeplace, e.getY() + yDeplace);
-            chessPiece.setSize(chessPiece.getWidth(), chessPiece.getHeight());
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
 
-            //Mise en "surbrillance" de la case
-            if(ech.getPiece(depart).estBlanc())
-                surbrillance(depart, ((ech.getTrait()=='w') ? Color.cyan : Color.red));
-            else
-                surbrillance(depart, ((ech.getTrait()=='w') ? Color.red : Color.cyan));
-            
-            //Mise en surbrillance des cases accessibles
-            afficheLesPositionsDansLeGUI(depart);
-            
-            layeredPane.add(chessPiece, JLayeredPane.DRAG_LAYER);
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (!first) {
+			Component c = chessBoard.findComponentAt(e.getX(), e.getY());
+			chessPiece.setVisible(false);
 
-            first = false;
-        }
-    }
+			JPanel panel = (JPanel) chessBoard.getComponent((8 - depart.getY() - 1) * ech.getDimX() + depart.getX());
 
-    @Override
-    public void mouseMoved(MouseEvent e) {
+			arrive = new Position((int) ((e.getX() / 600.0) * 8.0), (int) ((((600.0 - e.getY()) / 600.0) * 8.0)));
+			// here we have to save the color and the piece into d in order to
+			// later save it in the arraylist
 
-    }
+			Deplacement d = new Deplacement(depart, arrive, ech.getPiece(depart).getCode(),
+					ech.getPiece(depart).getColor());
 
-    @Override
-    public void mouseEntered(MouseEvent e) {
+			System.out.println("==> Déplacement : " + d);
 
-    }
+			if (makeDeplacement(d, c)) {
 
-    @Override
-    public void mouseExited(MouseEvent e) {
+			} else {
+				// replacer sur la case de départ
+				panel.add(chessPiece);
+				chessPiece.setVisible(true);
+			}
 
-    }
-    public JLayeredPane CreationChessboard(){
-    
-    	return layeredPane;
-    }
+			// Remettre la couleur d'origine
+			dessinEchiquier();
+
+			first = true;
+		} else {
+			chessPiece = null;
+			Component c = chessBoard.findComponentAt(e.getX(), e.getY());
+			// si la case est vide : rien à faire
+			if (c instanceof JPanel) {
+				return;
+			}
+
+			// retrouver la case correspondante
+			depart = new Position((int) ((e.getX() / 600.0) * 8.0), (int) ((((600.0 - e.getY()) / 600.0) * 8.0)));
+
+			System.out.print(depart);
+
+			Point parentLocation = c.getParent().getLocation();
+
+			xDeplace = parentLocation.x - e.getX();
+			yDeplace = parentLocation.y - e.getY();
+			chessPiece = (JLabel) c;
+			chessPiece.setLocation(e.getX() + xDeplace, e.getY() + yDeplace);
+			chessPiece.setSize(chessPiece.getWidth(), chessPiece.getHeight());
+
+			// Mise en "surbrillance" de la case
+			if (ech.getPiece(depart).estBlanc())
+				surbrillance(depart, ((ech.getTrait() == 'w') ? Color.cyan : Color.red));
+			else
+				surbrillance(depart, ((ech.getTrait() == 'w') ? Color.red : Color.cyan));
+
+			// Mise en surbrillance des cases accessibles
+			afficheLesPositionsDansLeGUI(depart);
+
+			layeredPane.add(chessPiece, JLayeredPane.DRAG_LAYER);
+
+			first = false;
+		}
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+
+	}
+
+	public JLayeredPane CreationChessboard() {
+
+		return layeredPane;
+	}
 }
